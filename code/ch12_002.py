fb4be27227b6cf85251d5b743d549f59eac130b0
Danil <denner02@mail.ru>
2022-05-28 21:10:25 +0300

    some changes

--- code/ch12_002.py
+++ code/ch12_002.py
@@ -1,11 +1,15 @@
 import numpy as np
-
+import sys, random, math
+from collections import Counter
 
 def softmax(x_):
     x = np.atleast_2d(x_)
     temp = np.exp(x)
     return temp / np.sum(temp, axis=1, keepdims=True)
 
+
+y = np.array([1,0,0,0,0,0,0,0,0])
+alpha = 0.01
 
 word_vects = {}
 word_vects['yankees'] = np.array([[0., 0., 0.]])
@@ -31,4 +35,22 @@ print(f"{layer_2=}")
 print(f"{layer_2.dot(sent2output)=}")
 pred = softmax(layer_2.dot(sent2output))
 print(pred)
+
+pred_delta = pred - y
+layer_2_delta = pred_delta.dot(sent2output.T)
+defeat_delta = layer_2_delta * 1
+layer_1_delta = layer_2_delta.dot(identity.T)
+sox_delta = layer_1_delta * 1
+layer_0_delta = layer_1_delta.dot(identity.T)
+
+word_vects['red'] -= layer_0_delta * alpha
+word_vects['sox'] -= sox_delta * alpha
+word_vects['defeat'] -= defeat_delta * alpha
+
+identity -= np.outer(layer_0, layer_1_delta) * alpha
+identity -= np.outer(layer_1, layer_2_delta) * alpha
+sent2output -= np.outer(layer_2, pred_delta)
+
+
+
 
