a96e2d43c77c1532c9270286d59c9d6d9f380aae
Danil <denner02@mail.ru>
2022-06-10 19:44:38 +0300

    begin of autograd

--- code/ch12_003.py
+++ code/ch12_003.py
@@ -2,6 +2,10 @@ import sys, math, random
 from collections import Counter
 import numpy as np
 
+from first_nn_framework.theta_net import Tensor
+
+print(Tensor())
+
 np.random.seed(1)
 
 embed_size = 10
@@ -111,7 +115,6 @@ for iter in range(50000):
             pred = vocab[each_layer['pred'].argmax()]
             print("Prev Input:" + input + (' ' * (12 - len(input))) + \
                   "True:" + true + (" " * (15 - len(true))) + "Pred:" + pred)
-
 
 
 
--- code/first_nn_framework/theta_net.py
+++ code/first_nn_framework/theta_net.py
@@ -1,12 +1,27 @@
 import numpy as np
 
+
 class Tensor(object):
 
-    def __init__(self, data):
+    def __init__(self, data, creators=None, creation_op=None):
         self.data = np.array(data)
+        self.creators = creators
+        self.creation_op = creation_op
+        self.grad = None
+
+    def backward(self, grad):
+        self.grad = grad
+
+        if (self.creation_op == "add"):
+            self.creators[0].backward(grad)
+            self.creators[1].backward(grad)
 
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        print(self)
+        print(other)
+        return Tensor(self.data + other.data,
+                      creators=[self, other],
+                      creation_op="add")
 
     def __repr__(self):
         return str(self.data.__repr__())
@@ -14,11 +29,14 @@ class Tensor(object):
     def __str__(self):
         return str(self.data.__str__())
 
-x = Tensor([1,2,3,4,5,6,7])
-print(f"{x=}")
-y = x + x
-print(f"{y=}")
-z = y + np.array([1,2,3,4,5,6,7])
-print(f"{z=}")
-k = z + Tensor([1,2,3,4,5,6,7])
-print(f"{k=}")
+
+x = Tensor([1,2,3,4,5])
+y = Tensor([2,2,2,2,2])
+
+z = x + y + x
+z.backward(Tensor(np.array([1,1,1,1,1])))
+
+print(x.grad)
+print(y.grad)
+print(z.creators)
+print(z.creation_op)
