210aac21278ec5204cb861d6a273ca58b56fdc4c
Danil <denner02@mail.ru>
2022-06-17 17:20:59 +0300

    Sequential test

--- code/first_nn_framework/theta_net_tests.py
+++ code/first_nn_framework/theta_net_tests.py
@@ -6,23 +6,18 @@ np.random.seed(0)
 data = Tensor(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]), autograd=True)
 target = Tensor(np.array([[0], [1], [0], [1]]), autograd=True)
 
-w = list()
-w.append(Tensor(np.random.rand(2, 3), autograd=True))
-w.append(Tensor(np.random.rand(3, 1), autograd=True))
+model = Sequential([Linear(2, 3), Linear(3, 1)])
 
-optim = SGD(parameters=w, alpha=0.1)
+optim = SGD(parameters=model.get_parameters(), alpha=0.05)
 
 for i in range(10):
+    pred = model.forward(data)
 
-    # Predict
-    pred = data.mm(w[0]).mm(w[1])
-
-    # Compare
     loss = ((pred - target) * (pred - target)).sum(0)
 
-    # Learn
     loss.backward(Tensor(np.ones_like(loss.data)))
 
     optim.step()
-
     print(loss)
+
+
