a04b20f98f817f3ba0f5e4c6fa256325efc8d951
Danil <denner02@mail.ru>
2022-06-27 16:21:37 +0300

    RNNCell test

--- code/first_nn_framework/theta_net_tests.py
+++ code/first_nn_framework/theta_net_tests.py
@@ -1,20 +1,94 @@
 from thetanet import *
+import sys, random, math
+from collections import Counter
 import numpy as np
 
-np.random.seed(0)
+f = open('../tasksv11/en/qa1_single-supporting-fact_train.txt', 'r')
+raw = f.readlines()
+f.close()
 
-# исходные индексы
-data = Tensor(np.array([1,2,1,2]), autograd=True)
-# целевые индексы
-target = Tensor(np.array([0,1,0,1]), autograd=True)
-model = Sequential([Embedding(3,3), Tanh(), Linear(3,4)])
+tokens = list()
+for line in raw[0:1000]:
+    tokens.append(line.lower().replace("\n", "").split(" ")[1:])
+
+new_tokens = list()
+for line in tokens:
+    new_tokens.append(['-'] * (6 - len(line)) + line)
+
+tokens = new_tokens
+
+vocab = set()
+for sent in tokens:
+    for word in sent:
+        vocab.add(word)
+
+vocab = list(vocab)
+
+word2index = {}
+for i, word in enumerate(vocab):
+    word2index[word] = i
+
+
+def words2indices(sentence):
+    idx = list()
+    for word in sentence:
+        idx.append(word2index[word])
+    return idx
+
+
+indices = list()
+for line in tokens:
+    idx = list()
+    for w in line:
+        idx.append(word2index[w])
+    indices.append(idx)
+
+data = np.array(indices)
+
+embed = Embedding(vocab_size=len(vocab), dim=16)
+model = RNNCell(n_inputs=16, n_hidden=16, n_output=len(vocab))
+
 criterion = CrossEntropyLoss()
-optim = SGD(parameters=model.get_parameters(), alpha=0.1)
-for i in range(20):
-    pred = model.forward(data)
+optim = SGD(parameters=model.get_parameters() + embed.get_parameters(), alpha=0.05)
 
-    loss = criterion.forward(pred, target)
-    loss.backward(Tensor(np.ones_like(loss.data)))
+for iter in range(1000):
+    batch_size = 1000
+    total_loss = 0
+
+    hidden = model.init_hidden(batch_size=batch_size)
+
+    for t in range(5):
+        input = Tensor(data[0:batch_size, t], autograd=True)
+        rnn_input = embed.forward(input=input)
+        output, hidden = model.forward(input=rnn_input, hidden=hidden)
+
+    target = Tensor(data[0:batch_size, t + 1], autograd=True)
+    loss = criterion.forward(output, target)
+    loss.backward()
     optim.step()
-    print(loss)
+    total_loss += loss.data
+    if (iter % 200 == 0):
+        p_correct = (target.data == np.argmax(output.data, axis=1)).mean()
+        print("Loss:", total_loss / (len(data) / batch_size), "% Correct:", p_correct)
+
+batch_size = 1
+hidden = model.init_hidden(batch_size=batch_size)
+for t in range(5):
+    input = Tensor(data[0:batch_size,t], autograd=True)
+    rnn_input = embed.forward(input=input)
+    output, hidden = model.forward(input=rnn_input, hidden=hidden)
+
+target = Tensor(data[0:batch_size,t+1], autograd=True)
+loss = criterion.forward(output, target)
+
+ctx = ""
+for idx in data[5:6][0][0:-1]:
+    ctx += vocab[idx] + " "
+print("Context:",ctx)
+print("True:",vocab[target.data[0]])
+print("Pred:", vocab[output.data.argmax()])
+
+
+
+
 
